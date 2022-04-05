0213ba04ddc2f5ec4a3f5b1c143749e81a26b474
double-slide <93881358+double-slide@users.noreply.github.com>
2022-04-05 06:32:25 +0000

    Add sql queries for BootcampX Queries 3 - Group By

---
+++ 3_queries_group-by/4_total_assignment_submissions.sql
@@ -0,0 +1,7 @@
+SELECT cohorts.name as cohort, count(assignment_submissions.*) as total_submissions
+FROM students
+JOIN cohorts ON cohorts.id = students.cohort_id
+JOIN assignment_submissions ON assignment_submissions.student_id = students.id
+GROUP BY cohorts.name
+ORDER BY total_submissions DESC;
+
