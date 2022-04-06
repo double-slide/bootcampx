50a6f5a068d97da204adf503b3e0075161c44e4b
double-slide <93881358+double-slide@users.noreply.github.com>
2022-04-06 00:10:33 +0000

    sql queries for teachers and assistance requests

---
+++ 4_queries/9_cohort_average_assistance_duration.sql
@@ -0,0 +1,9 @@
+SELECT AVG(total_duration) AS average_total_duration
+FROM (
+	SELECT SUM(completed_at - started_at) AS total_duration
+	FROM assistance_requests
+	JOIN students ON students.cohort_id = assistance_requests.student_id
+	JOIN cohorts ON cohorts.id = students.cohort_id
+	GROUP BY cohorts.name
+) as total_duration;
+
